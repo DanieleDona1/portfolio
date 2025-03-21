01dc10f218e0e510e817b2028b568578814d7d95
Daniele Dona <d.dona01@gmail.com>
2025-03-21 16:50:05 +0100

    Added typewriter

--- src/app/main_content/landing-page/landing-page.component.ts
+++ src/app/main_content/landing-page/landing-page.component.ts
@@ -1,15 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
 @Component({
   selector: 'app-landing-page',
   imports: [NavbarComponent],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.scss'
+  styleUrl: './landing-page.component.scss',
 })
+export class LandingPageComponent implements OnInit {
+  texts = [
+    ' Frontend Developer.',
+    ' I turn designs into code.',
+    ' Always learning, always building.',
+  ];
+  typingSpeed = 150;
+  deleteSpeed = 50;
+  pauseTime = 1350;
+  currentTextIndex = 0;
+  @ViewChild('textElement', { static: true }) textElementRef!: ElementRef;
 
-export class LandingPageComponent {
-  constructor(){
+  constructor() {}
+
+  ngOnInit(): void {
+    this.loop();
   }
 
+  typeText(text: string, callback: () => void): void {
+    let i = 0;
+    const type = () => {
+      if (i < text.length && this.textElementRef) {
+        this.textElementRef.nativeElement.textContent += text[i];
+        i++;
+        setTimeout(type, this.typingSpeed);
+      } else {
+        setTimeout(callback, this.pauseTime);
+      }
+    };
+    type();
+  }
 
+  deleteText(callback: () => void): void {
+    if (!this.textElementRef) return;
+
+    let text = this.textElementRef.nativeElement.textContent || '';
+    const erase = () => {
+      if (text.length > 1 && this.textElementRef) {
+        text = text.slice(0, -1);
+        this.textElementRef.nativeElement.textContent = text;
+        setTimeout(erase, this.deleteSpeed);
+      } else {
+        setTimeout(callback, this.pauseTime);
+      }
+    };
+    erase();
+  }
+
+  loop(): void {
+    const text = this.texts[this.currentTextIndex];
+    this.typeText(text, () =>
+      this.deleteText(() => {
+        this.currentTextIndex = (this.currentTextIndex + 1) % this.texts.length;
+        this.loop();
+      })
+    );
+  }
 }
