a263bb0997603675044f5434145970b95d165b4b
Daniele Dona <d.dona01@gmail.com>
2025-03-22 17:15:03 +0100

    Added landing page mobile responsiv

--- src/app/main_content/landing-page/navbar-mobile/navbar-mobile.component.ts
+++ src/app/main_content/landing-page/navbar-mobile/navbar-mobile.component.ts
@@ -2,14 +2,10 @@ import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
-import {MatMenuModule} from '@angular/material/menu';
-
 
 @Component({
   selector: 'app-navbar-mobile',
-  imports: [ MatIconModule, MatButtonModule, MatToolbarModule, MatSidenavModule, MatListModule, MatMenuModule ],
+  imports: [ MatIconModule, MatButtonModule, MatToolbarModule ],
   templateUrl: './navbar-mobile.component.html',
   styleUrl: './navbar-mobile.component.scss'
 })
