b43d9b02b70ffd3ed19db46163f25b3d9f7c71aa
Daniele Dona <d.dona01@gmail.com>
2025-03-25 09:57:42 +0100

    Generated all component

--- src/app/main_content/main-content.component.ts
+++ src/app/main_content/main-content.component.ts
@@ -3,10 +3,12 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { NavbarComponent } from './landing-page/navbar/navbar.component';
 import { WhyMeComponent } from './why-me/why-me.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
+import { MyProjectsComponent } from './my-projects/my-projects.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
 
 @Component({
   selector: 'app-main-content',
-  imports: [ LandingPageComponent, NavbarComponent, WhyMeComponent, MySkillsComponent ],
+  imports: [ LandingPageComponent, NavbarComponent, WhyMeComponent, MySkillsComponent, MyProjectsComponent, ContactMeComponent ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
