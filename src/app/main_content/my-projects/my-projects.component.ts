067b9089be8173a19695ebb61fa9d1ca5b4c278a
Daniele Dona <d.dona01@gmail.com>
2025-03-25 14:11:33 +0100

    Added my-projects slide in animation

--- src/app/main_content/my-projects/my-projects.component.ts
+++ src/app/main_content/my-projects/my-projects.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-my-projects',
@@ -6,6 +6,30 @@ import { Component } from '@angular/core';
   templateUrl: './my-projects.component.html',
   styleUrl: './my-projects.component.scss'
 })
-export class MyProjectsComponent {
+export class MyProjectsComponent implements AfterViewInit {
 
+  constructor() { }
+
+  ngAfterViewInit() {
+    // IntersectionObserver erstellen
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible'); // Sichtbar, also Klasse hinzufügen
+        }
+      });
+    }, { threshold: 0.5 }); // 50% des Elements müssen sichtbar sein
+
+    // Klassen 'slide-in-right' und 'slide-in-left' überwachen
+    const elementsRight = document.querySelectorAll('.slide-in-right');
+    const elementsLeft = document.querySelectorAll('.slide-in-left');
+
+    elementsRight.forEach((element) => {
+      observer.observe(element);
+    });
+
+    elementsLeft.forEach((element) => {
+      observer.observe(element);
+    });
+  }
 }
