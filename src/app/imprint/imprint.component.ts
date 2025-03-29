2c3e0f0b7de27318f8a44bd8a69ccf38f74944e7
Daniele Dona <d.dona01@gmail.com>
2025-03-29 22:42:54 +0100

    Changed links

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-imprint',
-  imports: [ TranslateModule, CommonModule ],
+  imports: [ TranslateModule, CommonModule, RouterLink ],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 
 
 @Component({
   selector: 'app-privacy-policy',
-  imports: [ TranslateModule, CommonModule ],
+  imports: [ TranslateModule, CommonModule, RouterLink ],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
