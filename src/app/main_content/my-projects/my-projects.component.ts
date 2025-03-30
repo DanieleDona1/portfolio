104b56be16a7e556636d6047a7e4062a65d03705
Daniele Dona <d.dona01@gmail.com>
2025-03-30 23:49:27 +0200

    Added ngFor for projects

--- src/app/main_content/my-projects/my-projects.component.ts
+++ src/app/main_content/my-projects/my-projects.component.ts
@@ -1,13 +1,43 @@
+import { CommonModule } from '@angular/common';
 import { Component, AfterViewInit } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-my-projects',
-  imports: [ TranslateModule ],
+  imports: [ TranslateModule, CommonModule ],
   templateUrl: './my-projects.component.html',
   styleUrl: './my-projects.component.scss'
 })
 export class MyProjectsComponent implements AfterViewInit {
+  projects = [
+    {
+      id: 'join',
+      name: 'Join',
+      technologies: 'JavaScript | HTML | CSS | Firebase',
+      description: 'MY_PROJECTS.LIST.0.DESCRIPTION',
+      imageSrc: '../../../assets/img/my_projects/join-gimp.png',
+      projectLink: './Join',
+      githubLink: 'https://github.com/DanieleDona1/Join',
+    },
+    {
+      id: 'el_pollo_loco',
+      name: 'El Pollo Loco',
+      technologies: 'JavaScript | HTML | CSS',
+      description: 'MY_PROJECTS.LIST.1.DESCRIPTION',
+      imageSrc: '../../../assets/img/my_projects/pollo_loco-gimp.png',
+      projectLink: './el_pollo_loco',
+      githubLink: 'https://github.com/DanieleDona1/ElPolloLoco',
+    },
+    {
+      id: 'pokedex',
+      name: 'Pokédex',
+      technologies: 'JavaScript | HTML | CSS',
+      description: 'MY_PROJECTS.LIST.2.DESCRIPTION',
+      imageSrc: '../../../assets/img/my_projects/pokedexneu-gimp.png',
+      projectLink: './Pokedex',
+      githubLink: 'https://github.com/DanieleDona1/Pokedex',
+    },
+  ];
 
   constructor() { }
 
