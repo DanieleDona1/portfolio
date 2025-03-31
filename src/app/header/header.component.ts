d60ff862152b652d36d6aa752716cca0bcf1046d
Daniele Dona <d.dona01@gmail.com>
2025-03-31 11:21:14 +0200

    Added header component imprint and privacy

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,64 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  imports: [TranslateModule, CommonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent implements OnInit {
+  deImageSrc = '../../assets/img/navbar/de.svg';
+  enImageSrc = '../../assets/img/navbar/en.svg';
+
+  activeLanguage: string = '';
+
+  ngOnInit(): void {
+    this.setInitialLanguage();
+  }
+
+  setInitialLanguage(): void {
+    this.changeLanguageImg(this.activeLanguage);
+  }
+
+  changeLanguageImg(language: string): void {
+    this.activeLanguage = language;
+
+    if (language === 'de') {
+      this.deImageSrc = '../../assets/img/navbar/de-active.svg';
+      this.enImageSrc = '../../assets/img/navbar/en.svg';
+    } else if (language === 'en') {
+      this.enImageSrc = '../../assets/img/navbar/en-active.svg';
+      this.deImageSrc = '../../assets/img/navbar/de.svg';
+    }
+  }
+
+  isActive(language: string): boolean {
+    return this.activeLanguage === language;
+  }
+
+  translate: TranslateService = inject(TranslateService);
+  translateWebsite(language: string) {
+    this.translate.use(language);
+    this.changeLanguageImg(language);
+  }
+
+  constructor(private router: Router) {}
+
+  getHeaderStyles() {
+    const isImprint: boolean = this.router.url.includes('/imprint');
+    const isPrivacy: boolean = this.router.url.includes('/privacy-policy');
+    console.log(isImprint);
+
+    const styles: { [key: string]: string } = {};
+
+    if (isImprint || isPrivacy) {
+      styles['display'] = 'flex';
+    } else {
+      styles['display'] = 'none';
+    }
+    return styles;
+  }
+}
